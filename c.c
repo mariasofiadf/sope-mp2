f040f6b135c9715404743dba69aabdcd1d286671
Pedro Figueiredo <up201904675@fe.up.pt>
2021-04-03 16:07:00 +0100

    Loading args and selecting random task with threads

--- c.c
+++ c.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h> 
+#include <sys/stat.h> 
+#define NTHREADS 10
 
 int nsecs;
 char* fifoname;
@@ -21,10 +27,48 @@ void free_vars(){
     //free(fifoname);
 }
 
-int main(int argc, char**argv)
-{
+
+void create_public_fifo(){
+    char myfifo[64];
+    sprintf(myfifo,"/tmp/%s", fifoname);
+    mkfifo(myfifo, 0666);
+}
+
+void *task_request(void *a) {
+    int* i = malloc(sizeof(int));
+    i = (int*)a;
+	printf("In thread PID: %d ; TID: %lu. ; Request: %d\n", getpid(), (unsigned long) pthread_self(), *i);
+
+	pthread_exit(a);	// no termination code
+}
+
+int main(int argc, char**argv){
+
     if(load_args(argc,argv))
         return 1;
+
+    srand(time(NULL));   // Initialization of random function, should only be called once.
+    
+    create_public_fifo();
+
+
+    int i;	// thread counter
+	pthread_t ids[NTHREADS];	// storage of (system) Thread Identifiers
+
+	printf("\nMain thread PID: %d ; TID: %lu.\n", getpid(), (unsigned long) pthread_self());
+
+    int* r;
+	// new threads creation
+	for(i=0; i<NTHREADS; i++) {
+        r = malloc(sizeof(int));
+        *r = rand()%9 + 1;
+		if (pthread_create(&ids[i], NULL, task_request, r) != 0)
+			exit(-1);	// here, we decided to end process
+	}
+	// wait for finishing of created threads
+
+	printf("\n");
+	pthread_exit(NULL);	// here, not really necessary...
     return 0;
 
 }
--- makefile
+++ makefile
@@ -1,4 +1,8 @@
-xmod: c.c
-	gcc -Wall -o c c.c
+CC = gcc
+CFLAGS = -Wall
+LIBS = -lpthread
+
+c: c.c
+	$(CC) -o c c.c $(LIBS)
 clean:
 	rm -f c
